1a3848ecd1431dbbed5ca67994594b69c40969f5
saar <saarzivon@gmail.com>
2018-02-10 19:32:56 +0200

    starting node chat app !

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.static(path.join(__dirname,'../public')));
+
+const server = app.listen(PORT,() =>{
+    console.log(`listening on port ${PORT}...`)
+});
